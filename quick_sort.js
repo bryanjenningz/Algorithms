1be55e7e9baf3a4c848955943d6c26ed36d3f7e1
Bryan Jennings <bryanjenningz@gmail.com>
2015-09-12 22:10:00 -0700

    Update quick_sort.js
    Add in-place quicksort

--- quick_sort.js
+++ quick_sort.js
@@ -49,3 +49,37 @@ function quickSort(array) {
          .concat(array.filter(value => value === array[0]))
          .concat(quickSort(array.filter(value => value > array[0])));
 }
+
+// In-place quickSort, O(1) space
+var swap = function(array, i, j) {
+  array[i] ^= array[j]; // because all the cool kids use xor instead of an extra variable
+  array[j] ^= array[i];
+  array[i] ^= array[j];
+};
+
+var quickSortInPlace = function(array, low, high) {
+  low = low || 0;
+  high = high || array.length - 1;
+  var lowEnd = low
+  var highStart = high; 
+  var pivot = array[low];
+  var middleCount = 1;
+  var i = low + 1;
+
+  while (i <= highStart) {
+    var value = array[i];
+    if (value < pivot) {
+      if (i !== lowEnd) { swap(array, i, lowEnd); }
+      lowEnd += 1;
+      i += 1;
+    } else if (value === pivot) {
+      middleCount += 1;
+      i += 1;
+    } else if (value > pivot) {
+      if (i !== highStart) { swap(array, i, highStart); }
+      highStart -= 1;
+    }
+  }
+  if (lowEnd !== low) { quickSortInPlace(array, low, lowEnd); }
+  if (highStart !== high) { quickSortInPlace(array, lowEnd + middleCount, high); }
+};
